7506f1416ec7e06a6e57b9d40fd3b870f681a4cd
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-05-04 19:22:34 +0500

    new update

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,25 +1,8 @@
 import { Outlet } from "react-router-dom";
-import bg from "../assets/bg.jpg";
-import { useEffect, useState } from "react";
-import music from "../assets/mc.mp3";
+// import bg from "../assets/bg.jpg";
+import bg from "../assets/bg2.png";
 
 export const Layout = () => {
-  const [audio] = useState(new Audio(music));
-  const [isMusicStarted, setIsMusicStarted] = useState(false);
-  // useEffect(() => {
-  //   const playMusic = () => {
-  //     if (!isMusicStarted) {
-  //       audio.loop = true;
-  //       audio.volume = 0.3;
-  //       audio.play().catch((err) => console.log("Autoplay blocked:", err));
-  //       setIsMusicStarted(true);
-  //     }
-  //   };
-
-  //   document.addEventListener("click", playMusic, { once: true });
-
-  //   return () => document.removeEventListener("click", playMusic);
-  // }, [isMusicStarted, audio]);
 
   return (
     <div className="w100 h100 df fdc aic layout">
